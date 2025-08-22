16e753a39404f811787ac0e1aaac63df637cc3e1
jiarundong <j30079061@china.huawei.com>
2025-08-22 17:18:05 +0800

    use k8s ci

--- scripts/prepare_deepep_in_container.sh
+++ scripts/prepare_deepep_in_container.sh
@@ -1,5 +1,3 @@
-ENV_DIR="/home/runner/.cache/env_sglang.sh"
-source \"$ENV_DIR\"
 cd ${GITHUB_WORKSPACE}
 bash build.sh
 pip install ${GITHUB_WORKSPACE}/output/deep_ep*.whl --no-cache-dir
--- tests/python/deepep/run_test.sh
+++ tests/python/deepep/run_test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-nproc_per_node=16
+nproc_per_node=8
 master_addr=`hostname -I|awk -F " " '{print$1}'`
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 port=17621
