e3cd1e0c339dffb752e4cf935444114d7b2f3dd2
jiarundong <j30079061@china.huawei.com>
2025-08-22 10:14:54 +0800

    use k8s ci

--- scripts/prepare_deepep_in_container.sh
+++ scripts/prepare_deepep_in_container.sh
@@ -4,4 +4,4 @@ cd ${GITHUB_WORKSPACE}
 bash build.sh
 pip install ${GITHUB_WORKSPACE}/output/deep_ep*.whl --no-cache-dir
 cd "$(pip show deep-ep | awk '/^Location:/ {print $2}')"
-ln -s deep_ep/deep_ep_cpp*.so"
+ln -s deep_ep/deep_ep_cpp*.so
