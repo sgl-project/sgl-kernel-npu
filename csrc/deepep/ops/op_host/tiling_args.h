b9fc175795d6d7a55e3b2ba794026db1ff66b981
lih827 <383084552@qq.com>
2025-09-11 11:35:10 +0800

    fix issue

--- csrc/deepep/ops/op_host/tiling_args.h
+++ csrc/deepep/ops/op_host/tiling_args.h
@@ -6,4 +6,4 @@ namespace Moe {
 constexpr uint64_t COMBINE_STATE_WIN_OFFSET = 3U * 1024UL * 1024UL;
 constexpr uint64_t NOTIFY_DISPATCH_WIN_OFFSET = 204U * 1024UL * 1024UL;
 }  // namespace Moe
-#endif
+#endif  // TILING_ARGS_H
