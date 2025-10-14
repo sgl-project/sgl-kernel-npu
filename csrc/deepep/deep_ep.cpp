532cea0eb3d97fb8b1ea6aab2eb52cd5a77444b6
Kaniel_Zhou <zhoudeyang@huawei.com>
2025-10-14 14:07:20 +0800

    1.update x to new_x

--- csrc/deepep/deep_ep.cpp
+++ csrc/deepep/deep_ep.cpp
@@ -670,8 +670,8 @@ std::vector<at::Tensor> Buffer::fused_deep_moe(const at::Tensor &x, const at::Te
 
     EXEC_NPU_CMD(aclnnFusedDeepMoe,
                  // input
-                 x, expert_ids, gmm1_permuted_weight, gmm1_permuted_weight_scale, gmm2_weight, gmm2_weight_scale,
-                 static_cast<const std::nullptr_t &>(nullptr), new_scales,
+                 new_x, this->new_topk_idx, gmm1_permuted_weight, gmm1_permuted_weight_scale, gmm2_weight,
+                 gmm2_weight_scale, static_cast<const std::nullptr_t &>(nullptr), new_scales,
                  // attr
                  hcom_ep_name, num_ranks, rank, num_experts, shared_expert_num, shared_expert_rank_num, quant_mode,
                  global_bs,
