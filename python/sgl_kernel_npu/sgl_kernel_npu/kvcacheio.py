256c0a0a60d79ea89970661ea4b0254152be4d21
shoufenghu <hushoufeng@huawei.com>
2025-11-05 17:24:58 +0800

    resolve reviews and add test cases for index_k_buffer

--- python/sgl_kernel_npu/sgl_kernel_npu/kvcacheio.py
+++ python/sgl_kernel_npu/sgl_kernel_npu/kvcacheio.py
@@ -1,6 +1,16 @@
+from enum import Enum
 from typing import Optional
 
 import torch
+
+
+class TransferDirection(Enum):
+    H2D = 1
+    D2H = 2
+
+
+class TransferFlag(Enum):
+    FAST2D = 2
 
 
 def transfer_kv_dim_exchange(
@@ -10,54 +20,14 @@ def transfer_kv_dim_exchange(
     host_k: torch.Tensor,
     device_v: torch.Tensor,
     host_v: torch.Tensor,
-    page_size: int = 128,
-    direction: int = 1,
-    flags: int = 2,
-):
-    """
-    In the L1 and L2 radix cache scenarios, perform batch copy of KV data between the device and the host.
-
-    Args:
-        device_indices: token indices in device
-        host_indices: token indices in host
-        device_k: k_buffer in device
-        host_k: k_buffer in host
-        device_v: v_buffer in device
-        host_v: v_buffer in host
-        page_size: page size
-        direction: only values 1 and 2 are supported: 1 indicates H2D, and 2 indicates D2H.
-        flags: only value 2 is supported, which indicates 2D data transfer via calling aclrtMemcpy2dAsync.
-    """
-    assert flags == 2
-    assert direction == 1 or direction == 2
-    torch.ops.npu.transfer_kv_dim_exchange(
-        device_k,
-        host_k,
-        device_v,
-        host_v,
-        device_indices,
-        host_indices,
-        page_size,
-        direction,
-        flags,
-    )
-
-
-def transfer_kv_dim_exchange_split(
-    device_indices: torch.Tensor,
-    host_indices: torch.Tensor,
-    device_k: torch.Tensor,
-    host_k: torch.Tensor,
-    device_v: torch.Tensor,
-    host_v: torch.Tensor,
     device_index_k: Optional[torch.Tensor] = None,
     host_index_k: Optional[torch.Tensor] = None,
     page_size: int = 128,
-    direction: int = 1,
-    flags: int = 2,
+    direction: TransferDirection = TransferDirection.H2D,
+    flags: TransferFlag = TransferFlag.FAST2D,
 ):
     """
-    In the mla L1 and L2 radix cache scenarios, perform batch copy of KV data between the device and the host.
+    In the L1 and L2 radix cache scenarios, perform batch copy of KV data between the device and the host.
 
     Args:
         device_indices: token indices in device
@@ -72,8 +42,6 @@ def transfer_kv_dim_exchange_split(
         direction: only values 1 and 2 are supported: 1 indicates H2D, and 2 indicates D2H.
         flags: only value 2 is supported, which indicates 2D data transfer via calling aclrtMemcpy2dAsync.
     """
-    assert flags == 2
-    assert direction == 1 or direction == 2
     torch.ops.npu.transfer_kv_dim_exchange(
         device_k,
         host_k,
@@ -82,8 +50,8 @@ def transfer_kv_dim_exchange_split(
         device_indices,
         host_indices,
         page_size,
-        direction,
-        flags,
+        direction.value,
+        flags.value,
     )
     if device_index_k is not None and host_index_k is not None:
         torch.ops.npu.transfer_kv_dim_exchange(
@@ -94,6 +62,6 @@ def transfer_kv_dim_exchange_split(
             device_indices,
             host_indices,
             page_size,
-            direction,
-            flags,
+            direction.value,
+            flags.value,
         )
