1b320c1f1fab259baa1873c9e93c7f0e8d5ca73c
Vitaly Tuzov <13571155+terfendail@users.noreply.github.com>
2025-11-12 19:47:16 +0300

    Updated write_cache test

--- tests/python/sgl_kernel_npu/test_mem_alloc.py
+++ tests/python/sgl_kernel_npu/test_mem_alloc.py
@@ -23,16 +23,24 @@ def write_cache_indices_golden(
             pt += extend_len
 
 def test_write_cache_indices():
-    hidden_size = 6144
-    input = torch.randn(3, hidden_size).to(torch.bfloat16).npu()
-    residual = torch.randn(3, hidden_size).to(torch.bfloat16).npu()
-    weight = torch.randn(hidden_size).to(torch.bfloat16).npu()
-    bias = torch.randn(hidden_size).to(torch.bfloat16).npu()
-    res1, res2 = write_cache_indices(input, residual, weight, bias, 1e-6)
-    ans1, ans2 = write_cache_indices_golden(input, residual, weight, bias, 1e-6)
+    batch_size = 16
+    pool_size = 64
+    content_len = 256
+    page_size = 2
+    req_to_token_tensor = torch.zeros((pool_size, context_len), dtype=torch.int32, device="npu")
+    req_to_token_tensor_ref = torch.zeros((pool_size, context_len), dtype=torch.int32, device="npu")
+    req_pool_indices = torch.tensor(torch.arange(page_size*batch_size, device=self.device).reshape(-1), dtype=torch.int64, device = "npu")
 
-    torch.testing.assert_close(res1, ans1, rtol=5e-3)
-    torch.testing.assert_close(res2, ans2, rtol=5e-3)
+    prefix_lens = torch.randint(16, (batch_size), dtype=torch.int64, device = "npu")
+    extend_lens = torch.randint(16, (batch_size), dtype=torch.int64, device = "npu")
+    seq_lens = prefix_lens + extended_lens
+
+    prefix_tensors = [torch.randint(256, (batch_size), dtype=torch.int64, device = "npu") for r in range(batch_size)]
+    out_cache_loc = torch.arange(page_size*sum(extend_lens), device="npu").reshape(-1)
+
+    write_cache_indices(out_cache_loc, req_pool_indices, prefix_lens, seq_lens, extend_lens, prefix_tensors, req_to_token_tensor)
+    write_cache_indices_golden(out_cache_loc, req_pool_indices, prefix_lens, seq_lens, extend_lens, prefix_tensors, req_to_token_tensor_ref)
+    torch.testing.assert_close(req_to_token_tensor, req_to_token_tensor_ref, rtol=5e-3)
 
 if __name__ == "__main__":
     test_write_cache_indices()
