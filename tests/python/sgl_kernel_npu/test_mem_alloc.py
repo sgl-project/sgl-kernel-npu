bf64154bc71072667f09ab34fd1f0ee824177abc
Vitaly Tuzov <13571155+terfendail@users.noreply.github.com>
2025-11-13 12:55:44 +0300

    Small update

--- tests/python/sgl_kernel_npu/test_mem_alloc.py
+++ tests/python/sgl_kernel_npu/test_mem_alloc.py
@@ -1,5 +1,5 @@
 import torch
-from sgl_kernel_npu.mem_cache import write_cache_indices
+from sgl_kernel_npu.mem_cache import write_cache_indices_npu
 
 def write_cache_indices_golden(
     out_cache_loc: torch.Tensor,
@@ -30,17 +30,17 @@ def test_write_cache_indices():
     req_to_token_tensor = torch.zeros((pool_size, content_len), dtype=torch.int32, device="npu")
     req_to_token_tensor_ref = torch.zeros((pool_size, content_len), dtype=torch.int32, device="npu")
 
-    req_pool_indices = torch.randint(pool_size, (batch_size,), dtype=torch.int64, device = "npu").reshape(-1)
+    req_pool_indices = (torch.randint(pool_size//batch_size, (batch_size,), dtype=torch.int64, device = "npu")+torch.arange(batch_size, dtype=torch.int64, device = "npu")*(pool_size//batch_size)).reshape(-1)
     prefix_lens = torch.randint(16, (batch_size,), dtype=torch.int64, device = "npu")
     extend_lens = torch.randint(16, (batch_size,), dtype=torch.int64, device = "npu")
     seq_lens = prefix_lens + extend_lens
 
     prefix_tensors = [torch.randint(256, (r,), dtype=torch.int64, device = "npu") for r in prefix_lens]
-    out_cache_loc = torch.arange(sum(extend_lens), device="npu").reshape(-1)
+    out_cache_loc = torch.arange(sum(extend_lens), dtype=torch.int64, device="npu").reshape(-1)
 
     write_cache_indices_golden(out_cache_loc, req_pool_indices, prefix_lens, seq_lens, extend_lens, prefix_tensors, req_to_token_tensor_ref)
-    write_cache_indices(out_cache_loc, req_pool_indices, prefix_lens, seq_lens, extend_lens, prefix_tensors, req_to_token_tensor)
-    torch.testing.assert_close(req_to_token_tensor, req_to_token_tensor_ref, rtol=5e-3)
+    write_cache_indices_npu(out_cache_loc, req_pool_indices, prefix_lens, seq_lens, extend_lens, prefix_tensors, req_to_token_tensor)
+    torch.testing.assert_close(req_to_token_tensor, req_to_token_tensor_ref)
 
 if __name__ == "__main__":
     test_write_cache_indices()
