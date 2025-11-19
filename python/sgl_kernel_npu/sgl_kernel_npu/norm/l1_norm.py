263217c8d550de8ad919181d6ba6082644d99399
c30031083 <chenxu140@huawei.com>
2025-11-19 17:49:34 +0800

    l1_norm optimize

--- python/sgl_kernel_npu/sgl_kernel_npu/norm/l1_norm.py
+++ python/sgl_kernel_npu/sgl_kernel_npu/norm/l1_norm.py
@@ -1,17 +1,28 @@
 import torch
 import triton
 import triton.language as tl
+from sgl_kernel_npu.utils.triton_utils import get_device_properties
 
 
 @triton.jit
-def l1_norm_kernel(input_ptr, output_ptr, hidden_size: tl.constexpr):
+def l1_norm_kernel(
+    input_ptr,
+    output_ptr,
+    batch_size,
+    hidden_size: tl.constexpr,
+    NUM_CORES: tl.constexpr,
+):
     pid = tl.program_id(0)
-    input_ptr += pid * hidden_size
-    output_ptr += pid * hidden_size
-    cols = tl.arange(0, hidden_size)
-    buffered_values = tl.load(input_ptr + cols)
-    buffered_values /= tl.sum(buffered_values)
-    tl.store(output_ptr + cols, buffered_values.to(tl.float32))
+    block_size = (batch_size - 1) // NUM_CORES + 1
+    row_begin = pid * block_size
+    row_end = tl.minimum((pid + 1) * block_size, batch_size)
+    for row_idx in range(row_begin, row_end):
+        cols = tl.arange(0, hidden_size)
+        buffered_values = tl.load(input_ptr + row_idx * hidden_size + cols)
+        buffered_values /= tl.sum(buffered_values)
+        tl.store(
+            output_ptr + row_idx * hidden_size + cols, buffered_values.to(tl.float32)
+        )
 
 
 def l1_norm(input):
@@ -20,6 +31,8 @@ def l1_norm(input):
     output = torch.empty(
         batch_size, hidden_size, device=input.device, dtype=torch.float32
     )
-
-    l1_norm_kernel[(batch_size,)](input, output, hidden_size)
+    _, num_vectorcore = get_device_properties()
+    l1_norm_kernel[(num_vectorcore,)](
+        input, output, batch_size, hidden_size, num_vectorcore
+    )
     return output
