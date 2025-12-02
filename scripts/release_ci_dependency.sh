cc65ac5100929447ffc0b98e55db0d5dae99c157
wangqi <2801406898@qq.com>
2025-12-02 19:41:27 +0800

    so目录测试

--- scripts/release_ci_dependency.sh
+++ scripts/release_ci_dependency.sh
@@ -27,3 +27,5 @@ ${PIP_INSTALL} torch==${PYTORCH_VERSION} torchvision==${TORCHVISION_VERSION} --i
 PTA_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com/sglang/torch_npu/torch_npu-2.8.0.post2.dev20251113-cp311-cp311-manylinux_2_28_aarch64.whl"
 ${PIP_INSTALL} ${PTA_URL}
 ${PIP_INSTALL} wheel==0.45.1 pybind11
+ASCEND_CANN_PATH=/usr/local/Ascend/ascend-toolkit
+source ${ASCEND_CANN_PATH}/set_env.sh
