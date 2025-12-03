5f1b7dab1cd782a933246179e82f0c06ce87937a
wangqi <2801406898@qq.com>
2025-12-03 16:27:27 +0800

    so目录测试

--- scripts/release_ci_dependency.sh
+++ scripts/release_ci_dependency.sh
@@ -30,6 +30,7 @@ PTA_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com/sglang/torch_npu/
 ${PIP_INSTALL} ${PTA_URL}
 ${PIP_INSTALL} wheel==0.45.1 pybind11
 find /usr -name "pybind11Config.cmake"
+find / -name types.h 2>/dev/null
 ASCEND_CANN_PATH=/usr/local/Ascend/ascend-toolkit
 source ${ASCEND_CANN_PATH}/set_env.sh
 #export LD_LIBRARY_PATH=${ASCEND_CANN_PATH}/latest/aarch64-linux/devlib/libascend_hal.so:$LD_LIBRARY_PATH
