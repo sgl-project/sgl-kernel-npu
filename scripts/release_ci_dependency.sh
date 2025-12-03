5249fddf2cc11e10ddadbe1a4d969d71ff7d574c
wangqi <2801406898@qq.com>
2025-12-03 10:04:31 +0800

    so目录测试

--- scripts/release_ci_dependency.sh
+++ scripts/release_ci_dependency.sh
@@ -28,7 +28,7 @@ ${PIP_INSTALL} torch==${PYTORCH_VERSION} torchvision==${TORCHVISION_VERSION} --i
 PTA_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com/sglang/torch_npu/torch_npu-2.8.0.post2.dev20251113-cp311-cp311-manylinux_2_28_aarch64.whl"
 ${PIP_INSTALL} ${PTA_URL}
 ${PIP_INSTALL} wheel==0.45.1 pybind11
-sudo find /usr -name "pybind11Config.cmake"
+find /usr -name "pybind11Config.cmake"
 ASCEND_CANN_PATH=/usr/local/Ascend/ascend-toolkit
 source ${ASCEND_CANN_PATH}/set_env.sh
 export LD_LIBRARY_PATH=${ASCEND_CANN_PATH}/latest/runtime/lib64/stub:$LD_LIBRARY_PATH
