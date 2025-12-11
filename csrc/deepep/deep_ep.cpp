51809302631fde4bc0efc7792758e60625172ca4
tianlunhu <htlshr@163.com>
2025-12-11 20:02:42 +0800

    add shmem implementation for low_latency moe

--- csrc/deepep/deep_ep.cpp
+++ csrc/deepep/deep_ep.cpp
@@ -845,7 +845,120 @@ Buffer::low_latency_dispatch(const at::Tensor &x, const at::Tensor &topk_idx,
             event,         std::function<void()>([] {})};
 }
 
-std::tuple<at::Tensor, std::optional<EventHandle>, std::optional<std::function<void()>>> Buffer::low_latency_combine(
+std::tuple<at::Tensor, std::optional<at::Tensor>, at::Tensor, at::Tensor, at::Tensor, std::optional<EventHandle>,
+           std::optional<std::function<void()>>>
+Buffer::shmem_low_latency_dispatch(const at::Tensor &x, const at::Tensor &topk_idx,
+                             const std::optional<at::Tensor> &cumulative_local_expert_recv_stats,
+                             int64_t num_max_dispatch_tokens_per_rank, int64_t num_experts, bool use_fp8,
+                             bool round_scale, bool use_ue8m0, bool async, bool return_recv_hook)
+{
+    this->is_padding = false;
+    EP_HOST_ASSERT(low_latency_mode);   //
+    at::Tensor new_x = x;
+    this->new_topk_idx = topk_idx;
+    if (topk_idx.size(0) < PADDING_SIZE) {
+        this->is_padding = true;
+        this->padding_cnt = PADDING_SIZE - topk_idx.size(0);
+        std::vector<at::Tensor> x_blocks;
+        std::vector<at::Tensor> topk_blocks;
+        if (topk_idx.size(0) != 0) {
+            x_blocks.emplace_back(x);
+            topk_blocks.emplace_back(topk_idx);
+        } else {
+            this->ori_x = x.clone();
+        }
+        int topk = static_cast<int>(new_topk_idx.size(1));
+        for (int i = 0; i < this->padding_cnt; i++) {
+            at::Tensor tmp_x = torch::ones({1, x.size(1)}, x.options());
+            at::Tensor tmp_topk = torch::arange(0, topk, topk_idx.options()).reshape({1, topk});
+            x_blocks.emplace_back(tmp_x);
+            topk_blocks.emplace_back(tmp_topk);
+        }
+        new_x = torch::cat(x_blocks, 0);
+        this->new_topk_idx = torch::cat(topk_blocks, 0);
+    }
+
+    auto num_tokens = static_cast<int>(new_x.size(0)), hidden = static_cast<int>(new_x.size(1));
+    auto num_scales = hidden / 128, num_topk = static_cast<int>(new_topk_idx.size(1));
+    auto num_local_experts = num_experts / (num_ranks - shared_expert_rank_num);
+
+    int64_t global_bs = std::max(new_topk_idx.size(0), num_max_dispatch_tokens_per_rank) * num_ranks;
+    auto num_max_tokens = 0;
+    if (rank < shared_expert_rank_num) {
+        num_max_tokens = global_bs / shared_expert_rank_num;
+        num_local_experts = 1;
+    } else {  // moe expert
+        num_max_tokens = global_bs * num_local_experts;
+    }
+    auto max_size = std::max(num_tokens * num_topk, num_max_tokens * 128);
+
+    // Allocate packed tensors
+    auto device = new_x.device();
+    auto packed_recv_x =
+        at::empty({num_max_tokens, hidden}, new_x.options().dtype(use_fp8 ? at::kChar : at::kBFloat16));
+    auto packed_recv_x_scales = at::empty({num_max_tokens}, at::dtype(at::kFloat).device(device));
+    // auto expandIdx = at::empty({max_size}, at::dtype(at::kInt).device(device));
+    auto expandIdx = at::empty({num_tokens * num_topk}, at::dtype(at::kInt).device(device));
+
+    int32_t server_num = num_ranks / LOCAL_RANK_SIZE;
+    at::Tensor ep_recv_count =
+        at::empty({num_local_experts * num_ranks}, at::dtype(at::kInt).device(device));  // A2 non-layered / A3
+    auto tp_recv_count = at::empty({1}, at::dtype(at::kInt).device(device));
+    auto packed_recv_count = at::empty({num_local_experts}, at::dtype(at::kLong).device(device));
+    at::Tensor scales;
+    at::Tensor active_mask;
+    int enable_neg_one = get_value_from_env("MOE_ENABLE_TOPK_NEG_ONE", 0);
+    int64_t quant_mode = use_fp8 ? 2 : 0;
+    int64_t tp_size = 1;
+    int64_t tp_rank = 0;
+    int64_t expert_shard_type = 0;
+    int outType = get_value_from_env("MOE_EXPERT_TOKEN_NUMS_TYPE", 1);
+    char *comm_alg;
+    int64_t expert_token_nums_type = outType;
+
+    // get shmem_ptr_info
+    int64_t ext_info = (int64_t)shmem_ptr;
+
+    // Wait streams
+    std::optional<EventHandle> event;
+    bool isLayered = false;
+
+    // do not support 910B at the moment
+    comm_alg = "fullmesh_v1";
+
+    if (enable_neg_one) {
+        EP_HOST_ASSERT(isLayered == false);
+        active_mask = (new_topk_idx >= 0).to(torch::kBool);
+    }
+
+    EXEC_NPU_CMD(aclnnShmemMoeDistributeDispatch, new_x, new_topk_idx,
+                 scales,        // smooth scales,
+                 active_mask,   // active_mask
+                 num_ranks,     // rankSize
+                 rank,          // rankId
+                 num_experts,
+                 tp_size,                 // tp_size
+                 tp_rank,                 // tp_rank
+                 expert_shard_type,       // expert_shard_type
+                 shared_expert_num,       // shared_expert_num
+                 shared_expert_rank_num,  // shared_expert_rank_num
+                 quant_mode,
+                 global_bs,               // global_bs
+                 expert_token_nums_type,  // expert_token_nums_type
+                 ext_info,                 // shmem_ptr as
+                 packed_recv_x,
+                 packed_recv_x_scales,  // dynamicScalesOut
+                 expandIdx,
+                 packed_recv_count,  // expertTokenNumsOut
+                 ep_recv_count, tp_recv_count);
+
+    // Return values
+    return {packed_recv_x, packed_recv_x_scales, packed_recv_count, expandIdx, ep_recv_count,
+            event, std::function<void()>([] {})};
+}
+
+std::tuple<at::Tensor, std::optional<EventHandle>, std::optional<std::function<void()>>>
+Buffer::low_latency_combine(
     const at::Tensor &x, const at::Tensor &topk_idx, const at::Tensor &topk_weights, const at::Tensor &src_info,
     const at::Tensor &layout_range, int64_t num_max_dispatch_tokens_per_rank, int64_t num_experts,
     const at::Tensor &packed_recv_count, bool zero_copy, bool async, bool return_recv_hook,
@@ -928,6 +1041,105 @@ std::tuple<at::Tensor, std::optional<EventHandle>, std::optional<std::function<v
                  shared_expert_x, hcom_ep_name, num_ranks, rank, num_experts, hcom_tp_name, tp_world_size, tp_rankId,
                  expert_shared_type, shared_expert_num, shared_expert_rank_num, global_bs, out_dtype, comm_quant_mode,
                  group_list_type, comm_alg, combined_x);
+    if (this->is_padding) {
+        if (this->padding_cnt == PADDING_SIZE) {
+            combined_x = this->ori_x;
+        } else {
+            combined_x = combined_x.slice(0, 0, PADDING_SIZE - this->padding_cnt);
+        }
+        is_padding = false;
+    }
+    return {combined_x, event, std::function<void()>([] {})};
+}
+
+std::tuple<at::Tensor, std::optional<EventHandle>, std::optional<std::function<void()>>>
+Buffer::shmem_low_latency_combine(
+    const at::Tensor &x, const at::Tensor &topk_idx, const at::Tensor &topk_weights, const at::Tensor &src_info,
+    const at::Tensor &layout_range, int64_t num_max_dispatch_tokens_per_rank, int64_t num_experts,
+    const at::Tensor &packed_recv_count, bool zero_copy, bool async, bool return_recv_hook,
+    const std::optional<at::Tensor> &out)
+{
+    at::Tensor new_idx = topk_idx;
+    at::Tensor new_scales = topk_weights;
+    if (this->is_padding) {
+        std::vector<at::Tensor> scales_blocks;
+        if (this->padding_cnt != PADDING_SIZE) {
+            scales_blocks.emplace_back(topk_weights);
+        }
+        for (int i = 0; i < this->padding_cnt; i++) {
+            at::Tensor tmp_scales = torch::zeros({1, topk_weights.size(1)}, topk_weights.options());
+            scales_blocks.emplace_back(tmp_scales);
+        }
+        new_idx = this->new_topk_idx;
+        this->new_scales = torch::cat(scales_blocks, 0);
+        new_scales = this->new_scales;
+    }
+    // Tensor checks
+    EP_HOST_ASSERT(x.dim() == 2 and x.is_contiguous() and x.scalar_type() == at::kBFloat16);
+    // EP_HOST_ASSERT(x.size(0) == num_experts / num_ranks);
+
+    // get shmem_ptr_info
+    int64_t ext_info = (int64_t)shmem_ptr;
+
+    auto device = x.device();
+    at::Tensor expand_x = x;
+    at::Tensor expert_ids = new_idx;
+    at::Tensor expand_idx = src_info;  // handle[0] = src_info
+    at::Tensor ep_send_counts = layout_range;
+    at::Tensor expert_scales = new_scales;
+    at::Tensor tp_send_counts = at::empty({1}, at::dtype(at::kInt).device(device));
+    at::Tensor x_active_mask, activation_scale, weight_scale, group_list, expand_scales;
+    int enable_neg_one = get_value_from_env("MOE_ENABLE_TOPK_NEG_ONE", 0);
+    int64_t tp_world_size = 1;
+    int64_t tp_rankId = 0;
+    int64_t expert_shared_type = 0;
+    int64_t global_bs = std::max(new_idx.size(0), num_max_dispatch_tokens_per_rank) * num_ranks;
+    int64_t out_dtype = 0;
+    int64_t comm_quant_mode = 0;
+    int64_t group_list_type = 0;
+    bool isLayered = false;
+    char *comm_alg;
+
+    auto num_combined_tokens = static_cast<int>(new_scales.size(0));
+    auto hidden = static_cast<int>(x.size(1));
+    // at::Tensor shared_expert_x{nullptr};
+    at::Tensor combined_x = at::empty({num_combined_tokens, hidden}, x.options());
+    std::optional<EventHandle> event;
+    // do not support 910B at the moment
+    comm_alg = "fullmesh_v1";
+
+    if (enable_neg_one) {
+        EP_HOST_ASSERT(isLayered == false);
+        x_active_mask = (new_topk_idx >= 0).to(torch::kBool);
+    }
+
+    EXEC_NPU_CMD(aclnnShmemMoeDistributeCombine,
+                 expand_x,
+                 expert_ids,
+                 expand_idx,
+                 ep_send_counts,
+                 expert_scales,
+                 tp_send_counts,
+                 x_active_mask,
+                 activation_scale,
+                 weight_scale,
+                 group_list,
+                 expand_scales,
+                 num_ranks,
+                 rank,
+                 num_experts,
+                 tp_world_size,
+                 tp_rankId,
+                 expert_shared_type,
+                 shared_expert_num,
+                 shared_expert_rank_num,
+                 global_bs,
+                 comm_quant_mode,
+                 ext_info,
+                 out_dtype,
+                 group_list_type,
+                 combined_x);
+
     if (this->is_padding) {
         if (this->padding_cnt == PADDING_SIZE) {
             combined_x = this->ori_x;
--- python/deep_ep/deep_ep/buffer.py
+++ python/deep_ep/deep_ep/buffer.py
@@ -686,6 +686,8 @@ class Buffer:
             hook,
         )
 
+        # noinspection PyTypeChecker
+
     @log_parameters(["topk_idx"])
     def low_latency_combine(
         self,
@@ -697,7 +699,8 @@ class Buffer:
         async_finish: bool = False,
         return_recv_hook: bool = False,
         out: Optional[torch.Tensor] = None,
-    ) -> Tuple[torch.Tensor, EventOverlap, Callable]:
+    ) -> Tuple[torch.Tensor, EventOverlap, Callable
+    ]:
         """
         A low-latency implementation for combine.
 
@@ -733,6 +736,184 @@ class Buffer:
             packed_recv_count,
         ) = handle
         combined_x, event, hook = self.runtime.low_latency_combine(
+            x,
+            topk_ids,
+            topk_weights,
+            src_info,
+            layout_range,
+            num_max_dispatch_tokens_per_rank,
+            num_experts,
+            packed_recv_count,
+            zero_copy,
+            async_finish,
+            return_recv_hook,
+            out,
+        )
+        tensors_to_record = (
+            x,
+            topk_idx,
+            topk_weights,
+            src_info,
+            layout_range,
+            combined_x,
+        )
+        return (
+            combined_x,
+            EventOverlap(event, tensors_to_record if async_finish else None),
+            hook,
+        )
+
+    @log_parameters(["topk_idx"])
+    def shmem_low_latency_dispatch(
+        self,
+        x: torch.Tensor,
+        topk_idx: torch.Tensor,
+        num_max_dispatch_tokens_per_rank: int,
+        num_experts: int,
+        cumulative_local_expert_recv_stats: Optional[torch.Tensor] = None,
+        use_fp8: bool = True,
+        round_scale: bool = False,
+        use_ue8m0: bool = False,
+        async_finish: bool = False,
+        return_recv_hook: bool = False,
+    ) -> Tuple[
+        Tuple[torch.Tensor, torch.Tensor], torch.Tensor, Tuple, EventOverlap, Callable
+    ]:
+        """
+        A low-latency implementation for dispatch.
+
+        Arguments:
+            x: `torch.Tensor` with `torch.bfloat16`, shaped as `[num_tokens, hidden]`, only several hidden shapes are
+                supported. The number of tokens to be dispatched must be less than `num_max_dispatch_tokens_per_rank`.
+            topk_idx: `torch.Tensor` with `torch.int64`, shaped as `[num_tokens, num_topk]`, only several top-k shapes
+                are supported. `-1` indices (not selecting any expert) are supported.
+            num_max_dispatch_tokens_per_rank: the maximum number of tokens to dispatch, all the ranks must hold the same value.
+            num_experts: the number of all experts.
+            cumulative_local_expert_recv_stats: a cumulative expert count tensor for statistics, which should have shape
+                `[num_local_experts]` and be typed as `torch.int`. This is useful for online service EP load balance
+                monitoring.
+            use_fp8: whether to enable FP8 casting, with this, the received data will be a tuple of FP8 tensor and scaling factors.
+            round_scale: whether round the scaling factors into power of 2.
+            use_ue8m0: whether use UE8M0 as scaling factor format (available only with `round_scale=True`).
+            async_finish: the current stream will not wait for the communication kernels to be finished if set.
+            return_recv_hook: return a receiving hook if set. If set, the kernel will just do the RDMA request issues,
+                but **without actually receiving the data**. You must call the received hook to make sure the data's arrival.
+                If you do not set this flag, the kernel will ensure the data's arrival.
+
+        Returns:
+            recv_x: a tensor or tuple with received tokens for each expert.
+                With `use_fp8=True`: the first element is a `torch.Tensor` shaped as
+                `[num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, hidden]` with `torch.float8_e4m3fn`.
+                The second tensor is the corresponding scales for the first element with shape
+                `[num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, hidden // 128]` with `torch.float`,
+                if `use_ue8m0=False`. With `use_ue8m0=True`, the second one is packed and shaped as
+                `[num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, hidden // 512]` with type `torch.int`.
+                Notice that, the last-two-dimension of the scaling tensors are in column-major for TMA compatibility.
+                With `use_fp8=False`, the result would be a tensor shaped as
+                `[num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, hidden]` with `torch.bfloat16`.
+                Moreover, not all tokens are valid, only some of the `num_max_dispatch_tokens_per_rank * num_ranks` are,
+                as we do not synchronize CPU received count with GPU (also not incompatible with CUDA graph if synced).
+            recv_count: a tensor shaped `[num_local_experts]` with type `torch.int`, indicating how many tokens each
+                expert receives. As mentioned before, not all tokens are valid in `recv_x`.
+            handle: the communication handle to be used in the `shmem_low_latency_combine` function.
+            event: the event after executing the kernel (valid only if `async_finish` is set).
+            hook: the receiving hook function (valid only if `return_recv_hook` is set).
+        """
+        topk_ids = topk_idx.int()
+        (
+            packed_recv_x,
+            packed_recv_x_scales,
+            packed_recv_count,
+            packed_recv_src_info,
+            packed_recv_layout_range,
+            event,
+            hook,
+        ) = self.runtime.shmem_low_latency_dispatch(
+            x,
+            topk_ids,
+            cumulative_local_expert_recv_stats,
+            num_max_dispatch_tokens_per_rank,
+            num_experts,
+            use_fp8,
+            round_scale,
+            use_ue8m0,
+            async_finish,
+            return_recv_hook,
+        )
+        handle = (
+            packed_recv_src_info,
+            packed_recv_layout_range,
+            num_max_dispatch_tokens_per_rank,
+            x.size(1),
+            num_experts,
+            packed_recv_count,
+        )
+        tensors_to_record = (
+            x,
+            topk_idx,
+            packed_recv_x,
+            packed_recv_x_scales,
+            packed_recv_count,
+            packed_recv_src_info,
+            packed_recv_layout_range,
+            cumulative_local_expert_recv_stats,
+        )
+        return (
+            (packed_recv_x, packed_recv_x_scales) if use_fp8 else packed_recv_x,
+            packed_recv_count,
+            handle,
+            EventOverlap(event, tensors_to_record if async_finish else None),
+            hook,
+        )
+
+    @log_parameters(["topk_idx"])
+    def shmem_low_latency_combine(
+        self,
+        x: torch.Tensor,
+        topk_idx: torch.Tensor,
+        topk_weights: torch.Tensor,
+        handle: tuple,
+        zero_copy: bool = False,
+        async_finish: bool = False,
+        return_recv_hook: bool = False,
+        out: Optional[torch.Tensor] = None,
+    ) -> Tuple[torch.Tensor, EventOverlap, Callable
+    ]:
+        """
+        A low-latency implementation for combine.
+
+        Arguments:
+            x: `[num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, hidden]` with `torch.bfloat16`,
+                the local calculated tokens to be sent to this original rank and reduced.
+            topk_idx: `[num_combined_tokens, num_topk]` with `torch.int64`, the expert indices selected by the dispatched
+                tokens. `-1` indices (not selecting any expert) are supported. Note that, `num_combined_tokens` equals
+                to the number of dispatched tokens.
+            topk_weights: `[num_combined_tokens, num_topk]` with `torch.float`, the expert weights selected by the dispatched
+                tokens. The received tokens will be reduced with the weights in this tensor.
+            handle: the communication handle given by the `dispatch` function.
+            zero_copy: whether the tensor is already copied into the RDMA buffer, should be cooperative
+                with `get_next_low_latency_combine_buffer`.
+            async_finish: the current stream will not wait for the communication kernels to be finished if set.
+            return_recv_hook: return a receiving hook if set. If set, the kernel will just do the RDMA request issues,
+                but **without actually receiving the data**. You must call the received hook to make sure the data's arrival.
+                If you do not set this flag, the kernel will ensure the data's arrival.
+            out: the in-place output tensor, if set, the kernel will write the result to this tensor and return it directly.
+
+        Returns:
+            combined_x: the reduced token tensor, with shape `[num_combined_tokens, hidden]` and type `torch.bfloat16`.
+            event: the event after executing the kernel (valid only if `async_finish` is set).
+            hook: the receiving hook function (valid only if `return_recv_hook` is set).
+        """
+        topk_ids = topk_idx.int()
+        (
+            src_info,
+            layout_range,
+            num_max_dispatch_tokens_per_rank,
+            hidden,
+            num_experts,
+            packed_recv_count,
+        ) = handle
+        combined_x, event, hook = self.runtime.shmem_low_latency_combine(
             x,
             topk_ids,
             topk_weights,
