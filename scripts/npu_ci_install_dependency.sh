42cb572c0172d203af0a1f596fede978f9ed9ddc
Even Zhou <even.y.zhou@outlook.com>
2025-12-23 20:43:37 +0800

    prepare build and release for a2

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -1,38 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
-PIP_INSTALL="pip install --no-cache-dir"
+export ARCHITECT="$(arch)"
+export DEBIAN_FRONTEND="noninteractive"
+export PIP_INSTALL="python3 -m pip install --no-cache-dir"
 
 
-# Install the required dependencies in CI.
-apt update -y && apt install -y \
+### Dependency Verisons
+TORCH_VERSION="2.8.0"
+TORCHVISION_VERSION="0.23.0"
+TORCH_NPU_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com/sglang/torch_npu/torch_npu-2.8.0.post2.dev20251113-cp311-cp311-manylinux_2_28_${ARCHITECT}.whl"
+
+
+### Install required dependencies
+## APT packages
+apt update -y && \
+apt upgrade -y && \
+apt install -y \
+    locales \
+    ca-certificates \
     build-essential \
     cmake \
+    ccache \
+    pkg-config \
+    zlib1g-dev \
     wget \
     curl \
-    net-tools \
-    zlib1g-dev \
-    lld \
-    clang \
-    locales \
-    ccache \
-    ca-certificates
+    zip \
+    unzip
+
+## Setup
+locale-gen en_US.UTF-8
 update-ca-certificates
-python3 -m ${PIP_INSTALL} --upgrade pip
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+
+## Python packages
+${PIP_INSTALL} --upgrade pip
+# Pin wheel to 0.45.1, REF: https://github.com/pypa/wheel/issues/662
+${PIP_INSTALL} \
+    wheel==0.45.1 \
+    pybind11
 
 
-### Install PyTorch and PTA
-PYTORCH_VERSION=2.6.0
-TORCHVISION_VERSION=0.21.0
-${PIP_INSTALL} torch==$PYTORCH_VERSION torchvision==$TORCHVISION_VERSION
-
-PTA_VERSION="v7.1.0.1-pytorch2.6.0"
-PTA_NAME="torch_npu-2.6.0.post1-cp311-cp311-manylinux_2_28_aarch64.whl"
-PTA_URL="https://gitee.com/ascend/pytorch/releases/download/${PTA_VERSION}/${PTA_NAME}"
-wget -O "${PTA_NAME}" "${PTA_URL}" && ${PIP_INSTALL} "./${PTA_NAME}"
-
-### Install Triton-Ascend
-TRITON_ASCEND_NAME="triton_ascend-3.2.0.dev20250729-cp311-cp311-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl"
-TRITON_ASCEND_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com/sglang/${TRITON_ASCEND_NAME}"
-${PIP_INSTALL} attrs==24.2.0 numpy==1.26.4 scipy==1.13.1 decorator==5.1.1 psutil==6.0.0 pytest==8.3.2 pytest-xdist==3.6.1 pyyaml pybind11
-wget -O "${TRITON_ASCEND_NAME}" "${TRITON_ASCEND_URL}" && ${PIP_INSTALL} "./${TRITON_ASCEND_NAME}"
+### Install pytorch
+## torch
+${PIP_INSTALL} \
+    torch==${TORCH_VERSION} \
+    torchvision==${TORCHVISION_VERSION} \
+    torchaudio==${TORCH_VERSION} \
+    --index-url https://download.pytorch.org/whl/cpu
+## torch_npu
+${PIP_INSTALL} ${TORCH_NPU_URL}
