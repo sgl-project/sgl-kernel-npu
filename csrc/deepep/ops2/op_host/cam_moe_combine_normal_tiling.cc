4ca3828c642d8055ea70b0d22198fefa931ee6db
zzx_passion <zhuzixiang2@h-partners.com>
2026-01-19 19:09:21 +0800

    The environment variable DEEPEP_HCCL_BUFFSIZE is added, and the priority of DEEPEP_HCCL_BUFFSIZE is higher than that of HCCL_BUFFSIZE.

--- csrc/deepep/ops2/op_host/cam_moe_combine_normal_tiling.cc
+++ csrc/deepep/ops2/op_host/cam_moe_combine_normal_tiling.cc
@@ -29,18 +29,20 @@ namespace {
 class Mc2TilingUtils
 {
 public:
-#define HCCL_BUFFSIZE "HCCL_BUFFSIZE"
     static uint64_t GetMaxWindowSize()
     {
         uint16_t defaultWindowSize = 200;
-        if (getenv(HCCL_BUFFSIZE) == nullptr) {
+        const char *hcclBuffSize = getenv("DEEPEP_HCCL_BUFFSIZE") == nullptr ? "HCCL_BUFFSIZE" : "DEEPEP_HCCL_BUFFSIZE";
+        if (getenv(hcclBuffSize) == nullptr) {
             OP_LOGD("", "Env HCCL_BUFFSIZE don't set");
         } else {
             try {
-                std::string envStr(getenv(HCCL_BUFFSIZE));
+                std::string envStr(getenv(hcclBuffSize));
                 defaultWindowSize = std::stoi(envStr);
-            } catch (...) {
-                OP_LOGE("", "Unknown Exception encountered when parser env HCCL_BUFFERSIZE");
+            } catch (const std::invalid_argument &ia) {
+                OP_LOGE("", "Invalid argument when parsing HCCL_BUFFSIZE: %s", ia.what());
+            } catch (const std::out_of_range &oor) {
+                OP_LOGE("", "Out of range when parsing HCCL_BUFFSIZE: %s", oor.what());
             }
         }
         const uint64_t maxWindowSize = static_cast<uint64_t>(defaultWindowSize) * 1024UL * 1024UL;
