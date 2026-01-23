baaecb81fb1138829c99286621d6e5ce89005667
zhuyy <zhuyutong4@h-partners.com>
2026-01-23 14:43:58 +0800

    Separate the tests under the two test_intranode conditions.

--- tests/python/deepep/test_low_latency.py
+++ tests/python/deepep/test_low_latency.py
@@ -132,6 +132,7 @@ def test(
             # Check received data
             recv_x = recv_x[:num_valid_tokens]
             recv_x_amin = recv_x[:, :-128].amin(dim=-1)
+            # In the presence of NaN, they are completely equal.
             assert torch.allclose(
                 recv_x_amin, recv_x[:, :-128].amax(dim=-1), equal_nan=True
             )
