31acd6dc61fb796fae25b02bda2404f0fe08a1ac
oagniqgnat <2275754866@qq.com>
2026-01-27 14:42:13 +0800

    Adaptation of the Deepep A5 normal and low-latency operators.

--- csrc/deepep/ops/op_host/op_api/aclnn_notify_dispatch.cpp
+++ csrc/deepep/ops/op_host/op_api/aclnn_notify_dispatch.cpp
@@ -2,6 +2,7 @@
 #include "graph/types.h"
 #include "aclnn_notify_dispatch.h"
 #include "aclnnInner_notify_dispatch.h"
+#include <stdio.h>
 
 extern void NnopbaseOpLogE(const aclnnStatus code, const char *const expr);
 
