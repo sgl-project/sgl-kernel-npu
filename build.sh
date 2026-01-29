3a9c86aec34c0ae7dbf02a5b1e668d9e61ad0366
探照灯 <51809194+1329009851@users.noreply.github.com>
2026-01-29 20:48:06 +0800

    Revert "Add scripts for building CMake files (#344)" (#353)
    This reverts commit 4c8011c84aec281f785003729d6cd8e80e0d1dfa.

--- build.sh
+++ build.sh
@@ -181,16 +181,6 @@ function build_memory_saver()
     cd -
 }
 
-function create_deepep_cmake()
-{
-    cd csrc || exit
-    chmod +x deepep_cmake_build.sh
-    chmod +x deepep/build.sh
-    chmod +x deepep/compile_ascend_proj.sh
-    ./deepep_cmake_build.sh
-    cd -
-}
-
 function make_deepep_package()
 {
     cd python/deep_ep || exit
@@ -219,7 +209,7 @@ function make_sgl_kernel_npu_package()
 
 function main()
 {
-    create_deepep_cmake
+
     build_kernels
     build_deepep_kernels
     if pip3 show wheel;then
