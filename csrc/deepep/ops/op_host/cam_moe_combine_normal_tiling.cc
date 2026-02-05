48dd4b952528107a575b59edbce6c7d62cdea3b3
luanyundu <49431685+luanyundu@users.noreply.github.com>
2026-02-05 14:31:34 +0800

    Fix the bug that total expert num greater than 256 or local expert num is less than 8 (#364)
    Co-authored-by: WSEmma <wusemma@163.com>

--- csrc/deepep/ops/op_host/cam_moe_combine_normal_tiling.cc
+++ csrc/deepep/ops/op_host/cam_moe_combine_normal_tiling.cc
@@ -398,7 +398,7 @@ static bool CheckTensorShape(gert::TilingContext *context, CamMoeCombineNormalTi
     int64_t xDim0 = xStorageShape->GetStorageShape().GetDim(0);
     int64_t xDim1 = xStorageShape->GetStorageShape().GetDim(1);
     OP_TILING_CHECK(xDim0 != topkWeightsDim0,
-                    OP_LOGE(nodeName, "x's dim0 is greater than bs, bs = %ld, x's dim0 = %ld", topkWeightsDim0, xDim0),
+                    OP_LOGE(nodeName, "x's dim0 not equal to bs, bs = %ld, x's dim0 = %ld", topkWeightsDim0, xDim0),
                     return false);
     OP_TILING_CHECK(xDim1 != recvXDim1,
                     OP_LOGE(nodeName, "x's dim1 not equal to h, x's dim1 = %ld, h = %ld", xDim1, recvXDim1),
