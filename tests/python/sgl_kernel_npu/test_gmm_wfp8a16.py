fix precision problems, update test cases and refactor

tests/python/sgl_kernel_npu/test_gmm_wfp8a16.py
@@ -69,7 +69,7 @@ def compare_data_Wf8Abf16(g, m, n, k):
     b_bf16 = anti_quant_fp8_to_bf16(b_int8, scales, g, k, n)
     c_fp32 = group_matmul(a_bf16, b_bf16, g, single_m)
 
-    result = torch.ops.npu.fp8_w8a16_grouped_matmul(
+    result = torch.ops.npu.softfp8_w8a16_grouped_matmul(
         a_bf16, b_int8, scales, group_list, "bf16"
     )
 