fb4979fa235f7a48705a8ed32cc55644a7d62f13
huangzhenyu <q_m_p@qq.com>
2026-03-28 14:26:36 +0800

    fix lint

--- csrc/cache_location_assign/op_host/cache_loc_assign.cpp
+++ csrc/cache_location_assign/op_host/cache_loc_assign.cpp
@@ -19,7 +19,6 @@
 namespace sglang {
 namespace npu_kernel {
 
-
 at::Tensor getTiling(const at::Tensor &reqPoolIndices, uint64_t rowSize, uint64_t poolSize, uint32_t &blockDim,
                      bool isUpddate)
 {
