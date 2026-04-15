781f965aa16277f0e99ed86505fb785a5dae147e
Svoloch <volochaevseraphim@gmail.com>
2026-04-15 17:53:37 +0300

    fix headers 1

--- csrc/attentions/csrc/ops/block_sparse_attention/op_host/block_sparse_attention_proto.cpp
+++ csrc/attentions/csrc/ops/block_sparse_attention/op_host/block_sparse_attention_proto.cpp
@@ -1,7 +1,8 @@
 /**
  * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- * MindIE is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * sgl-kernel-npu/svoloch2940194 is licensed under Mulan PSL v2.
+ * You can use this software acc
+ * ording to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *          http://license.coscl.org.cn/MulanPSL2
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
