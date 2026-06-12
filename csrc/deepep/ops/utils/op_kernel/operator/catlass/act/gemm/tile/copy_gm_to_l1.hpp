c9d8150cdae409db655792c3c808ecda65850163
Wenhao <ge.wen@northeastern.edu>
2026-06-12 17:14:53 +0800

    replaced srcOffset and dstOffset by srcOffset64 and dstOffset64

--- csrc/deepep/ops/utils/op_kernel/operator/catlass/act/gemm/tile/copy_gm_to_l1.hpp
+++ csrc/deepep/ops/utils/op_kernel/operator/catlass/act/gemm/tile/copy_gm_to_l1.hpp
@@ -615,7 +615,7 @@ struct CopyGmToL1<Arch::AtlasA2, Gemm::GemmType<Element, layout::RowMajor>,
                 AscendC::DataCopyParams dataCopyParams(rActual, cols / ELE_NUM_PER_BLK, srcStride, dstStride);
                 uint64_t dstOffset64 = (uint64_t)i * MAX_REPEAT * layoutDst.stride(0);
                 uint64_t srcOffset64 = (uint64_t)i * MAX_REPEAT * layoutSrc.stride(0);
-                DataCopy(dstTensor[dstOffset], srcTensor[srcOffset], dataCopyParams);
+                DataCopy(dstTensor[dstOffset64], srcTensor[srcOffset64], dataCopyParams);
             }
         } else {
             for (uint32_t i = 0; i < rows; ++i) {
