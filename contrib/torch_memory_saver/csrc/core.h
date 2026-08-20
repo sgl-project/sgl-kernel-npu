49d80e02fdd2fb1d3942bd9fd7e72a493c9e7e55
摆渡人 <2044145178@qq.com>
2026-08-20 09:43:34 +0800

    style: match CI clang-format output

--- contrib/torch_memory_saver/csrc/core.h
+++ contrib/torch_memory_saver/csrc/core.h
@@ -7,30 +7,31 @@
 #include <unordered_map>
 
 struct AllocationMetadata {
-    size_t size;
-    int device;
-    aclrtDrvMemHandle allocHandle;
-    std::string tag;
-    bool enable_cpu_backup;
-    void *cpu_backup;
+  size_t size;
+  int device;
+  aclrtDrvMemHandle allocHandle;
+  std::string tag;
+  bool enable_cpu_backup;
+  void *cpu_backup;
 };
 
 class TorchMemorySaver {
 public:
-    static TorchMemorySaver &instance();
+  static TorchMemorySaver &instance();
 
-    aclError malloc(void **ptr, int device, size_t size, const std::string &tag, bool enable_cpu_backup);
-    aclError free(void *ptr);
+  aclError malloc(void **ptr, int device, size_t size, const std::string &tag,
+                  bool enable_cpu_backup);
+  aclError free(void *ptr);
 
-    void pause(const std::string &tag);
-    void resume(const std::string &tag);
+  void pause(const std::string &tag);
+  void resume(const std::string &tag);
 
 private:
-    TorchMemorySaver();
-    ~TorchMemorySaver() = default;
-    TorchMemorySaver(const TorchMemorySaver &) = delete;
-    TorchMemorySaver &operator=(const TorchMemorySaver &) = delete;
+  TorchMemorySaver();
+  ~TorchMemorySaver() = default;
+  TorchMemorySaver(const TorchMemorySaver &) = delete;
+  TorchMemorySaver &operator=(const TorchMemorySaver &) = delete;
 
-    std::mutex allocator_metadata_mutex_;
-    std::unordered_map<void *, AllocationMetadata> allocation_metadata_;
+  std::mutex allocator_metadata_mutex_;
+  std::unordered_map<void *, AllocationMetadata> allocation_metadata_;
 };
